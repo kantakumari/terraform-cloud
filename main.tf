b4399aa4ff3fc13e3f9d41b0c43cd9aef827556c
kantakumari <kanta.kumari@publicissapient.com>
2023-01-27 20:54:16 +0530

    adding new files

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = var.project_name
+  region  = var.region
+  zone    = var.zone
+}
 resource "google_storage_bucket" "auto-expire" {
   name          = var.bucket_name
   location      = var.bucket_location
--- variables.tf
+++ variables.tf
@@ -1,7 +1,19 @@
+variable "project_name" {
+  type = string
+  default = "burner-kankumar8"
+}
+variable "region" {
+  type = string
+  default = "us-central1"
+}
 
+variable "zone" {
+  type = string
+  default = "us-central1-c"
+}
 variable "bucket_location" {
   type = string
-  default = "eastus"
+  default = "US"
 }
 variable "bucket_name" {
   type = string 
